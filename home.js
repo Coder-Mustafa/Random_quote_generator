bbcc1a0d197ca6ddd539ba6927ce273c7f2a1642
coder-mustafa <Mirmustafaali95@gmail.com>
2018-01-11 13:05:39 +0530

    changes to the website

--- home.js
+++ home.js
@@ -17,7 +17,9 @@ $(document).ready( function(){
 
     $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
       $("#quote-content").html(json.quoteText);
+
       $("#quote-author").html(json.quoteAuthor);
+      $("#tweet").attr('href',"https://twitter.com/intent/tweet?text=" + json.quoteText+"&hashtags="+json.quoteAuthor);
     });
 
 
